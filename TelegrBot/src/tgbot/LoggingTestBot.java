69a278ee860632354152a929ada11baecd4b5f75
Alexandr.M <enterplz@mail.ru>
2017-06-20 17:13:25 +0300

    add code for bot

---
+++ TelegrBot/src/tgbot/LoggingTestBot.java
@@ -0,0 +1,59 @@
+package tgbot;
+
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+class LoggingTestBot extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            // Set variables
+            String user_first_name = update.getMessage().getChat().getFirstName();
+            String user_last_name = update.getMessage().getChat().getLastName();
+            String user_username = update.getMessage().getChat().getUserName();
+            long user_id = update.getMessage().getChat().getId();
+            String message_text = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
+            String answer = message_text;
+            SendMessage message = new SendMessage() // Create a message object object
+                    .setChatId(chat_id)
+                    .setText(answer);
+
+            log(user_first_name, user_last_name, Long.toString(user_id), message_text, answer);
+
+            try {
+                sendMessage(message); // Sending our message object to user
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "SomeNewbie_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "400148210:AAEW705RnW__tWzjRsL3JAx8BhSa0Tf4KNs";
+    }
+
+    private void log(String first_name, String last_name, String user_id, String txt, String bot_answer) {
+        System.out.println("\n ----------------------------");
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        System.out.println(dateFormat.format(date));
+        System.out.println("Message from " + first_name + " " + last_name + ". (id = " + user_id + ") \n Text - " + txt);
+        System.out.println("Bot answer: \n Text - " + bot_answer);
+    }
+}
---
+++ TelegrBot/src/tgbot/SimpleBot.java
@@ -0,0 +1,23 @@
+package tgbot;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class SimpleBot {
+    public static void main(String[] args) {
+        // Initialize Api Context
+        ApiContextInitializer.init();
+
+        // Instantiate Telegram Bots API
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        // Register our bot
+        try {
+            botsApi.registerBot(new LoggingTestBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+        System.out.println("LoggingTestBot successfully started!");
+    }
+}
